2b922a54588727dab01186f6ba495f12d44ade47
Robert Guerra <robertjguerra@yahoo.com>
2026-02-19 11:32:25 -0800

    Initial Commit

---
+++ Youtine!/Youtine_App.swift
@@ -0,0 +1,32 @@
+//
+//  Youtine_App.swift
+//  Youtine!
+//
+//  Created by Bobby Guerra on 2/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Youtine_App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Youtine!Tests/Youtine_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Youtine_Tests.swift
+//  Youtine!Tests
+//
+//  Created by Bobby Guerra on 2/19/26.
+//
+
+import Testing
+@testable import Youtine_
+
+struct Youtine_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
